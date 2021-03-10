aca3a4f758483c5fc9b5553a2a0768d30fecf24b
Caleb Smith <smithcaleb042@gmail.com>
2021-03-10 15:07:17 -0700

    added new basemaps

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,24 @@
-let worldMap = L.map('worldmap').setView([51.505, -0.09], 13);
+let worldMap = L.map('worldmap').setView([20, 0], 3);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FsZWJycyIsImEiOiJja20zdmRvZjQwNm5tMnVueHhuaDIzdGllIn0._Myr90ISv0S773_l5GD2dQ', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY2FsZWJycyIsImEiOiJja20zdmRvZjQwNm5tMnVueHhuaDIzdGllIn0._Myr90ISv0S773_l5GD2dQ',
-}).addTo(worldMap);
+// let CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+// 	subdomains: 'abcd',
+// 	maxZoom: 19
+// });
+
+let CartoDB_VoyagerNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+// let mapBoxBaseMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FsZWJycyIsImEiOiJja20zdmRvZjQwNm5tMnVueHhuaDIzdGllIn0._Myr90ISv0S773_l5GD2dQ', {
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'mapbox/streets-v11',
+//   tileSize: 512,
+//   zoomOffset: -1,
+//   accessToken: 'pk.eyJ1IjoiY2FsZWJycyIsImEiOiJja20zdmRvZjQwNm5tMnVueHhuaDIzdGllIn0._Myr90ISv0S773_l5GD2dQ',
+// });
+
+CartoDB_VoyagerNoLabels.addTo(worldMap);
