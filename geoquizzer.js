16240cddf26b251b8d9ee17ac268bd5c604c9bbc
Caleb Smith <smithcaleb042@gmail.com>
2021-03-10 12:44:14 -0700

    First commit, initial setup

---
+++ geoquizzer.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
+
+app.get('/', (req, res) => {
+  res.render('map');
+});
+
+app.listen(port, host, () => {
+  console.log(`GeoQuizzer is listening on port ${port}...`);
+})
