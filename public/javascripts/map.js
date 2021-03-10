ed36326a4a7370d855d81fb88084030f05326a2a
Caleb Smith <smithcaleb042@gmail.com>
2021-03-10 16:00:55 -0700

    added points to three countries

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,38 @@
 let worldMap = L.map('worldmap').setView([20, 0], 3);
 
+let canadaPoint = {
+  "type": "Feature",
+  "properties": {
+      "name": "Canada",
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-98.307, 61.362]
+  }
+};
+
+let southAfricaPoint = {
+  "type": "Feature",
+  "properties": {
+      "name": "South Africa",
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [25.083, -29]
+  }
+};
+
+let mongoliaPoint = {
+  "type": "Feature",
+  "properties": {
+      "name": "Mongolia",
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [103.052, 46.826]
+  }
+};
+
 // let CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
 // 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 // 	subdomains: 'abcd',
@@ -22,3 +55,6 @@ let CartoDB_VoyagerNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/ras
 // });
 
 CartoDB_VoyagerNoLabels.addTo(worldMap);
+L.geoJSON(canadaPoint).addTo(worldMap);
+L.geoJSON(southAfricaPoint).addTo(worldMap);
+L.geoJSON(mongoliaPoint).addTo(worldMap);
